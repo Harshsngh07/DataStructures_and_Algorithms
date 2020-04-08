8c74fda3881e13d7fc43b7c505a2eb35046b8f71
Harsh Singh <harshsngh07@gmail.com>
2020-04-08 22:19:53 +0530

    random

--- CodeForces/Team.cpp
+++ CodeForces/Team.cpp
@@ -14,11 +14,11 @@ int main()
     for (int i = 0; i < n; i++)
         for (int j = 0; j < 3; j++)
         {
-            if (a[i][j] && (a[i][j + 1]) == 1)
+            if ((a[i][j] && (a[i][j + 1]) == 1) || ((a[i][j] && a[i][j + 2]) == 1) || ((a[i][j + 1] && a[i][j + 2]) == 1))
             {
                 submittedProblem++;
                 //cout << submittedProblem << endl;
-                //break;
+                break;
             }
         }
     cout << submittedProblem << endl;
