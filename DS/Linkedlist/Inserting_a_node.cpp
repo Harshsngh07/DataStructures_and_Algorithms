25e216c75cd9bbc440f1eb6fcf96a07958840a71
Harsh Singh <harshsngh07@gmail.com>
2020-04-09 23:36:40 +0530

    insert after a given node

--- DS/Linkedlist/Inserting_a_node.cpp
+++ DS/Linkedlist/Inserting_a_node.cpp
@@ -22,9 +22,32 @@ void pushNode(Node **head_ref, int new_data)
     (*head_ref) = new_node;
 }
 
+void insertAfter(Node *prev_node, int new_data)
+{
+    // check if the previous node is not null
+    if (prev_node == NULL)
+    {
+        cout << "Previous node cannot be Null";
+        return;
+    }
+
+    //allocate new node
+
+    Node *new_node = new Node();
+
+    //put in the data
+
+    new_node->data = new_data;
+
+    //Make next of new node to point to the next of previous node
+    new_node->next = prev_node->next;
+
+    //make next of prev node point to the new node
+
+    prev_node->next = new_node;
+}
+
 int main()
 {
-    Node* head = NULL;
-    
-
-    }
+    Node *head = NULL;
+}
