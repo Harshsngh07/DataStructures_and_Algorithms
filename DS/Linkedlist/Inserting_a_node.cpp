5e98793c9931ab764871867c6a65f0c26e21bd72
Harsh Singh <harshsngh07@gmail.com>
2020-04-09 23:51:43 +0530

    done

--- DS/Linkedlist/Inserting_a_node.cpp
+++ DS/Linkedlist/Inserting_a_node.cpp
@@ -75,7 +75,39 @@ void append(Node **head_ref, int new_data)
     last->next = new_node;
 }
 
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout << " " << n->data;
+        n = n->next;
+    }
+}
+
 int main()
 {
+    //start with the empty list
     Node *head = NULL;
+
+    //insert 6 linked list becomes 6->NULL
+    append(&head, 6);
+
+    //insert 7 at the beginning
+    //sp linked list becomes 7-> 6 -> Null
+    pushNode(&head, 7);
+
+    // Insert 1 at the beginning.
+    // So linked list becomes 1->7->6->NULL
+    push(&head, 1);
+
+    // Insert 4 at the end. So
+    // linked list becomes 1->7->6->4->NULL
+    append(&head, 4);
+
+    // Insert 8, after 7. So linked
+    // list becomes 1->7->8->6->4->NULL
+    insertAfter(head->next, 8);
+
+    cout << "Linkedlist is: ";
+    printList(head);
 }
