d255568b8a9e686fb2abccf10dadd2d12ca3b946
Harsh Singh <harshsngh07@gmail.com>
2020-04-11 03:06:38 +0530

    Stones_on_table.cpp

---
+++ CodeForces/Stones_on_table.cpp
@@ -0,0 +1,28 @@
+//g++  5.4.0
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    char stones[n];
+ 
+    for(int i=0;i<n;i++)
+        cin>>stones[i];
+
+    int count = 0;
+    int temp = stones[0];
+    for(int i=0;i<n;i++)
+    {
+       if (temp == stones[i+1])
+           count++;
+       temp = stones[i+1];
+   }
+   cout <<count;
+       
+
+
+
+}
