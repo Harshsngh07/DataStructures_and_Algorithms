a31f40a83f758fdfa7ac2514d065715c215743ab
Harsh Singh <harshsngh07@gmail.com>
2020-04-11 01:19:12 +0530

    minor changes

--- CodeForces/Word_cap.cpp
+++ CodeForces/Word_cap.cpp
@@ -9,7 +9,6 @@ int main()
     string word, temp;
     cin >> word;
     char first_char = toupper(word[0]);
-    //cout << first_char << endl;
     for (int i = 1; i < word.length(); i++)
         temp += word[i];
     cout << first_char << temp;
