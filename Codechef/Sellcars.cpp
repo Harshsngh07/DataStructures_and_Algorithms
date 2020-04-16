37d9e32e91836e77c2b3593bcac6e5e475cb3e12
Harsh Singh <harshsngh07@gmail.com>
2020-04-16 16:22:05 +0530

    new code

--- Codechef/Sellcars.cpp
+++ Codechef/Sellcars.cpp
@@ -11,21 +11,18 @@ int main()
     {
         long int n;
         cin >> n;
+        int mod = 1000000007;
         long long int arr[n];
         long long int profit = 0;
         for (int i = 0; i < n; i++)
-        {
             cin >> arr[i];
-        }
+
         sort(arr, arr + n, greater<int>());
         for (int i = 0; i < n; i++)
         {
-            arr[i] -= i;
-            if (arr[i] > 0)
-                profit += arr[i];
-
+            profit += max(0ll, arr[i] - i);
             // cout << arr[i];
         }
-        cout << profit << endl;
+        cout << profit % mod << endl;
     }
 }
