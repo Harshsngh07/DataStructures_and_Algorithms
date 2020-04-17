6567a53376e05eec763a203f62edee0f22efb0fa
Harsh Singh <harshsngh07@gmail.com>
2020-04-17 20:51:29 +0530

    new problems

---
+++ CodeForces/A/Petya_and_strings112.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#include <string>
+using namespace std;
+
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+#define endl "\n"
+#define sd(val) scanf("%d", &val)
+#define ss(val) scanf("%s", &val)
+#define sl(val) scanf("%lld", &val)
+#define debug(val) printf("check%d\n", val)
+#define all(v) v.begin(), v.end()
+#define PB push_back
+#define MP make_pair
+#define FF first
+#define SS second
+#define ll long long
+#define MOD 1000000007
+#define clr(val) memset(val, 0, sizeof(val))
+#define what_is(x) cerr << #x << " is " << x << endl;
+#define OJ                            \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+
+int main()
+{
+
+    OJ;
+    string s1, s2;
+    cin >> s1 >> s2;
+    transform(s1.begin(), s1.end(), s1.begin(), ::tolower);
+    transform(s2.begin(), s2.end(), s2.begin(), ::tolower);
+    if (s1 == s2)
+        cout << 0;
+    else if (s2 < s1)
+        cout << 1;
+    else if (s1 < s2)
+        cout << -1;
+    return 0;
+}
