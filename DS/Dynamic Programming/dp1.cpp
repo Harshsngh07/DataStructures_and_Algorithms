3e93ef079ee45c4d5c829dfea8d3942605588f03
Harsh Singh <harshsngh07@gmail.com>
2020-04-19 00:51:04 +0530

    added bottom up approach

--- DS/Dynamic Programming/dp1.cpp
+++ DS/Dynamic Programming/dp1.cpp
@@ -16,6 +16,35 @@ int fib(int n, int dp[])
     return dp[n] = ans;
 }
 
+//bottom up approach
+int fibBU(int n)
+{
+    int dp[100] = {0};
+    dp[1] = 1;
+    for (int i = 2; i <= n; i++)
+        dp[i] = dp[n - 1] + dp[n - 2];
+    return dp[n];
+}
+
+//bottom up approach with space optimisation
+
+int fiboBUSpace(int n)
+{
+    if (n == 0 or n == 1)
+        return n;
+
+    int a = 0;
+    int b = 1;
+    int c;
+    for (int i = 2; i <= n; i++)
+    {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
+}
+
 int main()
 {
     int n;
