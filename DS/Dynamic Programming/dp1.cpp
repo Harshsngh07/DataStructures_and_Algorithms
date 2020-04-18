08ad2dba188724b2c58adb62bff523259a1d5f02
Harsh Singh <harshsngh07@gmail.com>
2020-04-18 23:50:01 +0530

    recursion + memoisation technique added

--- DS/Dynamic Programming/dp1.cpp
+++ DS/Dynamic Programming/dp1.cpp
@@ -2,18 +2,24 @@
 using namespace std;
 
 //fib functions returns that nth element in the fibonacci series.
-int fib(int n)
+int fib(int n, int dp[])
 {
     //base case
     if (n == 0 or n == 1)
         return n;
-    int ans = fib(n - 1) + fib(n - 2);
-    return ans;
+
+    //look up recursion + memoisation
+    if (dp[n] != 0)
+        return dp[n];
+
+    int ans = fib(n - 1, dp) + fib(n - 2, dp);
+    return dp[n] = ans;
 }
 
 int main()
 {
     int n;
     cin >> n;
-    cout << fib(n);
+    int dp[100] = {0};
+    cout << fib(n, dp);
 }
