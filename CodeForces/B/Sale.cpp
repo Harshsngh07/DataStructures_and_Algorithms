25d0416214edc75ab0e794308ab20f5be11c0dcc
Harsh Singh <harshsngh07@gmail.com>
2020-04-22 00:51:12 +0530

    new problem

---
+++ CodeForces/B/Sale.cpp
@@ -0,0 +1,20 @@
+//g++  5.4.0
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n, m;
+    cin >> n >> m;
+    int a[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+
+    sort(a, a + n);
+    int sum(0);
+    for (int i = 0; i < m; i++)
+        if (a[i] <= 0)
+            sum += fabs(a[i]);
+    cout << sum;
+}
