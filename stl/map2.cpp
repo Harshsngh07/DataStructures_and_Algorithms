1e986e2ed51b771a6910d9f707023251e0ff0a5b
Harsh Singh <harshsngh07@gmail.com>
2020-05-07 00:52:06 +0530

    Update map2.cpp

--- stl/map2.cpp
+++ stl/map2.cpp
@@ -1,5 +1,3 @@
-//g++  5.4.0
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -13,39 +11,47 @@ int main()
         cin >> n>>q;
         string s; cin >> s;
         map<char , int> m;
-        
-        
+
+
         for(int i=0;i<s.length();i++)
             m[s[i]]++;
-        
+
         map<char,int> :: iterator it;
-        
+
         int total(0);
         //int a[q];
         while(q--)
         {   
             long long int x;
             cin >> x;
-        
-       
-        
+
+
+
                 for(int j=0;j<x;j++)
                 {
                    for( it=m.begin();it!=m.end();++it)
                        it->second -= 1;
-                   
                     
+                   for(it=m.begin();it!=m.end();++it)
+                   {
+                       if(it->second == 0)
+                           m.erase(it);
+                   }
+                   
+                   
                 }
-                 for( it=m.begin();it!=m.end();++it)
-                      total+=it->second;
-            
-                if(total<0)
+                
+                for(it = m.begin();it!=m.end();++it)
+                       total+=it->second;
+
+                /*if(total<0)
                     cout<<0<<endl;
                 else
-                    cout<<total<<endl;
+                    cout<<total<<endl;*/
+              cout<<total<<endl;
         }
-        
+
         //cout<<fabs(total)<<endl;
     }
-    
+
 }
