63e600552a21d905ae62ec99a65ed0852d630a2b
Harsh Singh <harshsngh07@gmail.com>
2020-05-07 01:26:50 +0530

    changed

--- stl/map2.cpp
+++ stl/map2.cpp
@@ -3,55 +3,52 @@ using namespace std;
 
 int main()
 {
-     long long int t;
+    long long int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
-        long long int n,q;
-        cin >> n>>q;
-        string s; cin >> s;
-        map<char , int> m;
+        long long int n, q;
+        cin >> n >> q;
+        string s;
+        cin >> s;
 
-
-        for(int i=0;i<s.length();i++)
-            m[s[i]]++;
-
-        map<char,int> :: iterator it;
+        map<char, int> m;
+        map<char, int>::iterator it;
 
         int total(0);
-        //int a[q];
-        while(q--)
-        {   
+        int a[q];
+        for (int i = 0; i < q; i++)
+        {
             long long int x;
             cin >> x;
 
+            for (int i = 0; i < s.length(); i++)
+                m[s[i]]++;
 
+            while (x--)
+            {
 
-                for(int j=0;j<x;j++)
-                {
-                   for( it=m.begin();it!=m.end();++it)
-                       it->second -= 1;
-                    
-                   for(it=m.begin();it!=m.end();++it)
-                   {
-                       if(it->second == 0)
-                           m.erase(it);
-                   }
-                   
-                   
-                }
-                
-                for(it = m.begin();it!=m.end();++it)
-                       total+=it->second;
+                for (it = m.begin(); it != m.end(); ++it)
+                    it->second -= 1;
 
-                /*if(total<0)
+                for (it = m.begin(); it != m.end(); ++it)
+                    if (it->second == 0)
+                        m.erase(it);
+            }
+
+            for (it = m.begin(); it != m.end(); ++it)
+                total += it->second;
+
+            /*if(total<0)
                     cout<<0<<endl;
                 else
                     cout<<total<<endl;*/
-              cout<<total<<endl;
+            cout << total << endl;
+
+            /*for(it=m.begin();it!=m.end();++it)
+                cout<<it->first << " " <<it->second<<endl;*/
         }
 
         //cout<<fabs(total)<<endl;
     }
-
 }
