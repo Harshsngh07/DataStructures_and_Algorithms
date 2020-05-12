9c18a3a6098eff51214e07e7a06cc1ce54a5bee3
Harsh Singh <harshsngh07@gmail.com>
2020-05-12 14:44:02 +0530

    Create Day12_singleElement_in_sorted_array.cpp

---
+++ leetcode/MayChallenge/Day12_singleElement_in_sorted_array.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int res;
+        for(int i=0;i<nums.size();i++)
+            res^=nums[i];
+        return res;
+    }
+};
