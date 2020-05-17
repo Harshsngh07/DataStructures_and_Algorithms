077b4b935fc9d15d3783528bda984955b53c2f4e
Harsh Singh <harshsngh07@gmail.com>
2020-05-17 16:10:15 +0530

    index calculating in kadanes algorithm

--- DS/Dynamic Programming/Kadane'sAlgorithm.cpp
+++ DS/Dynamic Programming/Kadane'sAlgorithm.cpp
@@ -10,6 +10,7 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL);
+
 // function to return the max sum in an array of both +ve and -ve integers.
 int maxSubarray(int a[], int size)
 {
@@ -27,12 +28,38 @@ int maxSubarray(int a[], int size)
     return max_sofar;
 }
 
+//returns the index also
+int maxSubarraySum(int a[], int size)
+{
+    int max_sofar(INT_MIN), max_ending(0), start(0), end(0), s(0);
+
+    for (int i = 0; i < size; i++)
+    {
+        max_ending += a[i];
+        if (max_ending < a[i])
+        {
+            max_ending = a[i];
+            s = i + 1;
+        }
+
+        if (max_sofar < max_ending)
+        {
+            max_sofar = max_ending;
+            start = s;
+            end = i;
+        }
+    }
+    cout << "starting index " << start << endl
+         << "ending index " << end << endl;
+    return max_sofar;
+}
+
 int main()
 {
 
     int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
     int n = sizeof(a) / sizeof(a[0]);
-    int maxSum = maxSubarray(a, n);
+    int maxSum = maxSubarraySum(a, n);
     cout << "Maximum contiguous sum is " << maxSum << endl;
     return 0;
 }
