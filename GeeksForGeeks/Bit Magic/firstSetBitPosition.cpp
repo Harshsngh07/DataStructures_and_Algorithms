c9580e2ba46eb0e8ee3d120f7d475b056111f0f2
Harsh Singh <harshsngh07@gmail.com>
2020-05-29 01:23:40 +0530

    position of first set bit

---
+++ GeeksForGeeks/Bit Magic/firstSetBitPosition.cpp
@@ -0,0 +1,16 @@
+unsigned int getFirstSetBit(int n)
+{
+
+    // Your code here
+    int pos(1);
+    for (int i = 0; i < 32; i++)
+    {
+        if (!(n & (1 << i)))
+            pos++;
+        else
+            break;
+    }
+    if (n == 0)
+        return 0;
+    return pos;
+}
