9e3fa69ee38c33efdc4adcd27f98770345b2f4b7
Harsh Singh <harshsngh07@gmail.com>
2020-06-13 00:53:08 +0530

    minElement in O(1) space

--- GeeksForGeeks/Stack ds/minElementfromStack.cpp
+++ GeeksForGeeks/Stack ds/minElementfromStack.cpp
@@ -31,3 +31,42 @@ int _stack ::pop()
 
 //approach 2 in O(1) space
 
+int _stack :: getMin()
+{
+    if(s.empty())
+        return -1;
+    else
+        return minEle;
+}
+int _stack ::pop()
+{
+    if(s.empty())
+    {
+        return -1;
+    }
+    int t = s.top();
+    s.pop();
+    if(t<minEle)
+    {
+      int k = minEle;
+        minEle = 2*minEle-t;
+        return k;
+
+    }else
+    return t;
+}
+void _stack::push(int x)
+{
+    if(s.empty())
+    {
+        minEle =x ;
+        s.push(x);
+        return ;
+    }
+    if(x<minEle)
+    {
+        s.push(2*x-minEle);
+        minEle = x;
+    }else
+    s.push(x);
+}
