755604489dc814cac12bcf3ce748dd60e4f8a945
Harsh Singh <harshsngh07@gmail.com>
2020-07-14 11:52:09 +0530

    day13

--- leetcode/JulyChallenge/Day13_SameTree.cpp
+++ leetcode/JulyChallenge/Day13_SameTree.cpp
@@ -1,15 +1,7 @@
 class Solution {
 public:
     
-    void Inorder(vector<int>& v, TreeNode* node)
-    {
-        if(node == NULL) return;
-        
-        Inorder(v,node->left);
-        v.push_back(node->val);
-        Inorder(v, node->right);
-    }
-    
+   
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(p==NULL || q==NULL) return p==q;
         if(p->val!=q->val) return false;
