bab768106a17823087c2a4d2b8eb2b0f84572bc8
Harsh Singh <harshsngh07@gmail.com>
2020-08-09 18:15:15 +0530

    max simultaneous diff contest 3 problem

---
+++ GeeksForGeeks/Contest/InterviewPrep/Sorting/MaxSimultaneousDiff.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+	int t;
+	cin >> t;
+	while(t--)
+	{
+	    int n;
+	    cin >> n;
+	    int a[n];
+	    for(int i=0;i<n;i++) cin >> a[i];
+	    sort(a,a+n);
+	    int max = INT_MIN;
+	    
+	    for(int i=0;i<n-1;i++)
+	    {
+	        int x = abs(a[i]-a[i+1]);
+	        if(x>max)
+	            max = x; 
+	    }
+	    
+	    cout << max << endl;
+	}
+	return 0;
+}
