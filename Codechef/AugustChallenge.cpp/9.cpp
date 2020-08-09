109e3a79da96b07c3c4284875b14a48b62ac895b
Harsh Singh <harshsngh07@gmail.com>
2020-08-09 16:42:43 +0530

    chef and thanos div1 ques non scorable problem

---
+++ Codechef/AugustChallenge.cpp/9.cpp
@@ -0,0 +1,21 @@
+// non scorable problem
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long t;
+	cin >> t;
+	while(t--)
+	{
+	    long long n, m;
+	    cin >> n >> m;
+	    while(m--)
+	    {
+	        int a, b;
+	        cin >> a >> b;
+	        cout << 0 << endl;
+	    }
+	}
+	return 0;
+}
