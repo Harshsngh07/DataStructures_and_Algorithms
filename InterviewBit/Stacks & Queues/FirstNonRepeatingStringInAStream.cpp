90dcc464be01380d225da52c1429f455ac54682e
Harsh Singh <harshsngh07@gmail.com>
2020-09-12 23:39:40 +0530

    first non repeating character in a stream

---
+++ InterviewBit/Stacks & Queues/FirstNonRepeatingStringInAStream.cpp
@@ -0,0 +1,12 @@
+string res = "";
+queue<char> q;
+vector<int> m(256, 0);
+for (int i = 0; i < A.size(); i++)
+{
+    m[A[i]]++;
+    q.push(A[i]);
+    while (!q.empty() && m[q.front()] > 1)
+        q.pop();
+    res += !q.empty() ? q.front() : '#';
+}
+return res;
