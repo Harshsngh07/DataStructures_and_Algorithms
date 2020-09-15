e638fcb28f38bbcc792e7b5bcf6dc1cf888e503f
Harsh Singh <harshsngh07@gmail.com>
2020-09-15 17:28:10 +0530

    day 15 lc

---
+++ leetcode/SeptemberChallenge/Day15_lengthoflastword.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int n = s.length();
+        int ans(0);
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]!=' ') ans++;
+            else if(ans) return ans;
+        }
+      return ans;
+        
+    }
+};
