41b0df05d0424de2e908a7d30a23266d52c8669f
Harsh Singh <harshsngh07@gmail.com>
2020-09-20 14:29:44 +0530

    subarray with sum equal to k

---
+++ leetcode/HashTable/subarrayWithSumEqualToK.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        unordered_map<int, int> map;
+        map[0] = 1;
+        int count = 0;
+        int sum = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i)
+        {
+            sum += nums[i];
+            int rem = sum - k;
+            if (map.count(rem))
+            {
+                count += map[rem];
+            }
+            map[sum]++;
+        }
+        return count;
+    }
+};
+
+static int fastio = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
