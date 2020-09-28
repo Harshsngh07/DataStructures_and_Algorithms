a820bdbc5b5e94b07727f29e5ae6987aac5c602a
Harsh Singh <harshsngh07@gmail.com>
2020-09-28 22:52:07 +0530

    282

---
+++ CodeForces/A/282.cpp
@@ -0,0 +1,64 @@
+/* Author: Harsh Singh */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define fo(a, b) for (int i = a; i < b; i++)
+#define vec vector<int>
+#define vec_ll vector<long long>
+#define stk stack<int>
+#define stk_ll stack<long long>
+#define st set<int>
+#define st_ll set<long long>
+#define u_set unordered_set<int>
+#define u_set_ll unordered_set<long long>
+#define mp map<int, int>
+#define mp_c map<char, int>
+#define mp_ll map<long long, long long>
+#define u_map unordered_map<int, int>
+#define u_map_ll unordered_map<long long, long long>
+#define pq priority_queue<int>
+#define min_pq priority_queue<int, vector<int>, greater<int>>
+#define dq deque<int> 
+#define dq_ll deque<long long>
+#define pr pair<int, int>
+#define pr_ll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define nl endl
+
+typedef long long ll;
+
+int main()
+{
+    FIO;
+    int n;
+    cin >> n;
+    vector<string> s;
+    while(n--)
+    {
+        string str;
+        cin >> str;
+        s.pb(str);
+    }
+    
+    int ans(0);
+    for(int i=0;i<s.size();i++)
+    {
+        if(s[i]=="X++")
+            ans++;
+        else if(s[i]=="++X")
+            ++ans;
+        else if(s[i]=="--X")
+            --ans;
+        else
+            ans--;
+    }
+    cout << ans << endl;
+}
