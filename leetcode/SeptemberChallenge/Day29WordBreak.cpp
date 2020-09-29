65a57c890b8ee50e08377d14d3f247557371dff4
Harsh Singh <harshsngh07@gmail.com>
2020-09-29 22:51:05 +0530

    Day 29

---
+++ leetcode/SeptemberChallenge/Day29WordBreak.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    bool wordBreak(string s, vector<string> &wordDict)
+    {
+        int n = s.length();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (dp[j] && find(wordDict.begin(), wordDict.end(), s.substr(j, i - j)) != wordDict.end())
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+static int fastio = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}();
