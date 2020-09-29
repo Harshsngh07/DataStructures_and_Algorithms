34c37fa8894a28e4e9900639543f8625b005ac25
Harsh Singh <harshsngh07@gmail.com>
2020-09-29 21:49:02 +0530

    149A

---
+++ CodeForces/A/149.cpp
@@ -0,0 +1,68 @@
+/* Author: Harsh Singh */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MOD 1000000007
+#define FIO                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL);
+#define fo(a, b) for (int i = a; i < b; i++)
+#define vec vector<int>
+#define vec_ll vector<long long>
+#define stk stack<int>
+#define stk_ll stack<long long>
+#define st set<int>
+#define st_ll set<long long>
+#define u_set unordered_set<int>
+#define u_set_ll unordered_set<long long>
+#define mp map<int, int>
+#define mp_c map<char, int>
+#define mp_ll map<long long, long long>
+#define u_map unordered_map<int, int>
+#define u_map_ll unordered_map<long long, long long>
+#define pq priority_queue<int>
+#define min_pq priority_queue<int, vector<int>, greater<int>>
+#define dq deque<int> 
+#define dq_ll deque<long long>
+#define pr pair<int, int>
+#define pr_ll pair<long long, long long>
+#define pb push_back
+#define ff first
+#define ss second
+#define nl endl
+
+typedef long long ll;
+
+int solve(int k, int a[], int n)
+{
+    sort(a,a+n,greater<int>());
+    if(k==0)
+        return 0;
+    
+    int sum = accumulate(a,a+n,0);
+    if(k>sum)
+        return -1;
+    
+    int ans (0);
+    int i=0;
+    while(k>0)
+    {
+        k-=a[i++];
+        ans++;
+    }
+    
+    return ans;
+}
+
+int main()
+{
+    FIO;
+    int k;
+    cin >> k;
+    int n = 12;
+    int a[n];
+    fo(0,n) cin >> a[i];
+    cout << solve(k,a,n);
+}
