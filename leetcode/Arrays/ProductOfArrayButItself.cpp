3febeb13ce3096b17d8081a4d922391b9f51373f
Harsh Singh <harshsngh07@gmail.com>
2020-11-02 12:00:38 +0530

    product of array

--- leetcode/Arrays/ProductOfArrayButItself.cpp
+++ leetcode/Arrays/ProductOfArrayButItself.cpp
@@ -1,21 +1,23 @@
-class Solution {
+class Solution
+{
 public:
-    vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> productExceptSelf(vector<int> &nums)
+    {
         int n = nums.size();
-        vector<int> ans(n,1);
+        vector<int> ans(n, 1);
         int i, temp = 1;
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
             ans[i] = temp;
             temp *= nums[i];
         }
         temp = 1;
-        for(int i=n-1;i>=0;i--)
+        for (int i = n - 1; i >= 0; i--)
         {
             ans[i] *= temp;
             temp *= nums[i];
         }
-        
+
         return ans;
-  }
+    }
 };
