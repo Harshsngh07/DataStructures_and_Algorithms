6e60f0a420a869ff9ebbcb9a593266da635cbe02
Harsh Singh <harshsngh07@gmail.com>
2020-11-17 00:38:50 +0530

    Remove duplicate letters LC

---
+++ leetcode/Strings/RemoveDuplicateLetters.cpp
@@ -0,0 +1,44 @@
+class Solution
+{
+public:
+    string smallestSubsequence(string s)
+    {
+        int n = s.length();
+        int lastIndex[26];
+        memset(lastIndex, -1, sizeof(lastIndex));
+
+        for (int i = 0; i < n; i++)
+        {
+            lastIndex[s[i] - 'a'] = i;
+        }
+
+        bool visited[26];
+        memset(visited, false, sizeof(visited));
+
+        stack<char> stk;
+        for (int i = 0; i < n; i++)
+        {
+            if (visited[s[i] - 'a'] == true)
+                continue;
+            while (!stk.empty() && s[i] < stk.top() && lastIndex[stk.top() - 'a'] > i)
+            {
+                visited[stk.top() - 'a'] = false;
+                stk.pop();
+            }
+            stk.push(s[i]);
+            visited[s[i] - 'a'] = true;
+        }
+
+        string ans = "";
+
+        while (!stk.empty())
+        {
+            ans += stk.top();
+            stk.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
