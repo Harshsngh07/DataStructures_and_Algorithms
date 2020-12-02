cfa6d796c398aa0ffb6ff0682b0bd8f8c531e4de
Harsh Singh <harshsngh07@gmail.com>
2020-12-02 16:14:34 +0530

    Day2 LinkedList Random node

---
+++ leetcode/DecemberChallenge/Day2_LinkedListRandomNode.cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+public:
+    /** @param head The linked list's head.
+        Note that the head is guaranteed to be not null, so it contains at least one node. */
+    ListNode *head = NULL;
+    //int ans = 0;
+    Solution(ListNode *h)
+    {
+        head = h;
+    }
+
+    int getRandom()
+    {
+        ListNode *curr = head;
+        ListNode *res;
+        int n = 1;
+        while (curr)
+        {
+            if (rand() % n == 0)
+                res = curr;
+            curr = curr->next;
+            n++;
+        }
+        return res->val;
+    }
+};
