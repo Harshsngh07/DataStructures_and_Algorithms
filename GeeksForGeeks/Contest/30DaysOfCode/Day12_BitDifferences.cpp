89945901456ba65aedad24e05cdeb5c55ab55588
Harsh Singh <harshsngh07@gmail.com>
2021-02-06 22:37:15 +0530

    day 12 bit difference gfg

---
+++ GeeksForGeeks/Contest/30DaysOfCode/Day12_BitDifferences.cpp
@@ -0,0 +1,20 @@
+class Solution{
+    public:
+    long long sumBitDiff(int arr[], int n) 
+    { 
+        long long ans=0;
+        for(int i=0;i<32;i++)
+        {
+            long long count(0);
+            for(int j=0;j<n;j++)
+            {
+                if((arr[j] & (1<<i)))
+                    count++;
+            }
+            
+            ans += (count * (n-count)*2);
+        }
+        
+        return ans;
+    } 
+};
