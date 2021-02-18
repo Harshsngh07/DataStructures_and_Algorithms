db6dc5442564911981094ca9c96a07a204f066fd
Harsh Singh <harshsngh07@gmail.com>
2021-02-18 17:05:52 +0530

    Top K frequent words

---
+++ leetcode/Heaps/TopKFrequentWords.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+    vector<string> topKFrequent(vector<string> &words, int k)
+    {
+        unordered_map<string, int> m;
+        for (string s : words)
+            m[s]++;
+
+        priority_queue<pair<int, string>> pq;
+
+        for (auto i : m)
+        {
+            pq.push({-i.second, i.first});
+            if (pq.size() > k)
+                pq.pop();
+        }
+
+        vector<string> v;
+        while (k--)
+        {
+            v.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        reverse(v.begin(), v.end());
+        return v;
+    }
+};
