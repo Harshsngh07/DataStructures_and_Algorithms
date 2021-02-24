b889cfec6c536130521bb1f0e826a455bc085c67
Harsh Singh <harshsngh07@gmail.com>
2021-02-24 20:17:38 +0530

    day 24 score of parentheses

---
+++ leetcode/FebChallenge/Day24_ScoreOfParentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int scoreOfParentheses(string S) {
+        stack<int> s;
+        int score(0);
+        for(int i=0;i<S.length();i++)
+        {
+            if(S[i]=='(')
+            {
+                s.push(score);
+                score = 0;
+            }
+            else{
+                score = s.top()+ max(score*2, 1) ;
+                s.pop();
+                
+            }
+        }
+        return score;
+    }
+};
