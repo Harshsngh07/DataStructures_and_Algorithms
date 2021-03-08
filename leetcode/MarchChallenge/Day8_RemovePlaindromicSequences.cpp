2776b1d25154dda5b3cbc558395f9c2c6335bccd
Harsh Singh <harshsngh07@gmail.com>
2021-03-08 22:15:29 +0530

    day 8 lc remove plaindromic sequences

---
+++ leetcode/MarchChallenge/Day8_RemovePlaindromicSequences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s)
+    {
+        int left(0), right(s.length()-1);
+        while(left<right)
+        {
+            if(s[left]!=s[right])
+                return false;
+            left++;
+            right--;
+        }
+        
+        return true;
+    }
+    int removePalindromeSub(string s) {
+        if(s.length()==0) return 0;
+        
+        if(isPalindrome(s)) return 1;
+        
+        return 2;
+    }
+};
