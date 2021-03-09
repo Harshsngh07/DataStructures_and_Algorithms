f90e0f672fc682948dd95667fe545880c7f0226c
Harsh Singh <harshsngh07@gmail.com>
2021-03-09 13:46:36 +0530

    permutation sequence

--- leetcode/MarchChallenge/Day8_RemovePlaindromicSequences.cpp
+++ leetcode/MarchChallenge/Day8_RemovePlaindromicSequences.cpp
@@ -1,23 +1,27 @@
-class Solution {
+class Solution
+{
 public:
     bool isPalindrome(string s)
     {
-        int left(0), right(s.length()-1);
-        while(left<right)
+        int left(0), right(s.length() - 1);
+        while (left < right)
         {
-            if(s[left]!=s[right])
+            if (s[left] != s[right])
                 return false;
             left++;
             right--;
         }
-        
+
         return true;
     }
-    int removePalindromeSub(string s) {
-        if(s.length()==0) return 0;
-        
-        if(isPalindrome(s)) return 1;
-        
+    int removePalindromeSub(string s)
+    {
+        if (s.length() == 0)
+            return 0;
+
+        if (isPalindrome(s))
+            return 1;
+
         return 2;
     }
 };
